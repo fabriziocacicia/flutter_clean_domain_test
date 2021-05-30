1c8ac9e2291d028e3016aa8b3301f22389b2dcfe
Fabrizio Cacicia <mail@fabriziocacicia.com>
2021-05-30 20:23:48 +0200

    feat: move the sources in the src directory and add the barrel files with the exports (#2)

--- lib/flutter_clean_domain_test.dart
+++ lib/flutter_clean_domain_test.dart
@@ -1,1 +1,4 @@
 library flutter_clean_domain_test;
+
+export 'helpers.dart';
+export 'mocks.dart';
---
+++ lib/helpers.dart
@@ -0,0 +1,4 @@
+library helpers;
+
+export './src/helpers/test_failure.dart';
+export './src/helpers/test_value_object.dart';
