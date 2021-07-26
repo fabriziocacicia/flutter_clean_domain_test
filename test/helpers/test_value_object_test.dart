944c57c4f61d0d7625633418242334ae91d9079f
Fabrizio Cacicia <mail@fabriziocacicia.com>
2021-07-26 21:04:09 +0200

    fix: fix the test of the value getter in the testValidValueObject function not passing in some cases (#9)
    * fix: change the test of the value getter since the equals matcher has problems with Right and Left classes
    
    * test: add a more complex test to test the testValidValueObject function
    
    * chore: add the changes to the unreleased changes in the changelog

--- test/helpers/test_value_object_test.dart
+++ test/helpers/test_value_object_test.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_domain/flutter_clean_domain.dart';
 import 'package:flutter_clean_domain_test/flutter_clean_domain_test.dart';
@@ -16,12 +18,30 @@ class MockValueFailure extends ValueFailure<int> {
   MockValueFailure(int invalidValue) : super(invalidValue: invalidValue);
 }
 
+class HashSetIntValueObject extends ValueObject<HashSet<int>> {
+  final Either<ValueFailure<HashSet<int>>, HashSet<int>> _value;
+
+  HashSetIntValueObject(Either<ValueFailure<HashSet<int>>, HashSet<int>> value)
+      : _value = value;
+
+  @override
+  Either<ValueFailure<HashSet<int>>, HashSet<int>> get value => _value;
+}
+
 void main() {
   group("testValidValueObject", () {
     final validValue = 0;
     final validValueObject = MockValueObject(Right(validValue));
 
     testValidValueObject<int>(object: validValueObject, value: validValue);
+
+    final validHashSet = HashSet<int>.from(<int>{1, 2, 3});
+    final validHashSetValueObject = HashSetIntValueObject(Right(validHashSet));
+
+    testValidValueObject(
+      object: validHashSetValueObject,
+      value: validHashSet,
+    );
   });
 
   group("testInvalidValueObjectButFailures", () {
