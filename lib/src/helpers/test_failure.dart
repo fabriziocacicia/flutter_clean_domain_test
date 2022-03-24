a55c0d610ed686ddbe31ccb8a75ac64a2979fa9a
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-03-24 18:12:09 +0100

    refactor: add dart_lint_rules and apply its rules (#14)

--- lib/src/helpers/test_failure.dart
+++ lib/src/helpers/test_failure.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_clean_domain/value_failure.dart';
 import 'package:test/test.dart';
 
+/// An helper function to test a [ValueFailure].
+/// It accepts the raw [invalidValue] and the [failure] containing it.
 void testValueFailure<T extends Object>({
   required T invalidValue,
   required ValueFailure<T> failure,
--- lib/src/mocks/validator_mock.dart
+++ lib/src/mocks/validator_mock.dart
@@ -3,5 +3,6 @@ import 'package:flutter_clean_domain/value_failure.dart';
 
 import 'function_mock.dart';
 
+/// A class to mock a validator function.
 class ValidatorMock<T extends Object>
     extends FunctionMock<Either<ValueFailure<T>, T>, T> {}
