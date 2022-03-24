ef956ad2a9280c36feab6478ea24b92b6a5ac172
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-03-24 18:09:31 +0100

    refactor: add dart_lint_rules and apply its rules

--- lib/src/mocks/function_mock.dart
+++ lib/src/mocks/function_mock.dart
@@ -3,5 +3,6 @@ import 'package:mocktail/mocktail.dart';
 /// Mocks a standalone function
 // ignore: one_member_abstracts
 abstract class FunctionMock<R extends Object, T extends Object> extends Mock {
+  // ignore: public_member_api_docs
   R call(T value);
 }
