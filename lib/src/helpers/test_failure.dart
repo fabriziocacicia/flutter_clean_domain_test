f38fe8bd803fa609cf6136304d60523750232ec3
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-03-24 18:51:39 +0100

    refactor: remove 'isA ValueFailure' test in test_failure helper
    The failure parameter will always be a ValueFailure since it's enforced by the parameter type.

--- lib/src/helpers/test_failure.dart
+++ lib/src/helpers/test_failure.dart
@@ -7,10 +7,6 @@ void testValueFailure<T extends Object>({
   required T invalidValue,
   required ValueFailure<T> failure,
 }) {
-  test("should be a ValueFailure", () {
-    expect(failure, isA<ValueFailure>());
-  });
-
   test("should contain the invalid value", () {
     expect(failure.invalidValue, invalidValue);
   });
